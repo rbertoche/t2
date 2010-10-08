0e68013a19623c71009623a77d24737919e35300
Raphael Bertoche <rbertoche@cpti.cetuc.puc-rio.br>
2010-10-08 02:07:45 -0300

    Move de fato a posse dos Nodes: Da lista 'nodes' pro 'graph'. Conserta delecoes

--- graph/graph.c
+++ graph/graph.c
@@ -50,7 +50,8 @@ Graph *GraphNew (FDelData fdd)
 	g = (Graph *) malloc(sizeof(Graph));
 	if (g!)
 		return NULL;
-	g->nodes = LIS_CriarLista(delNode);
+	/* Atencao, NULL abaixo esta correto. O Dono dos nodes e' o graph */
+	g->nodes = LIS_CriarLista(NULL);
 	if (!g->nodes){
 		return NULL;
 	}
@@ -99,7 +100,7 @@ enum graphRet GraphNewNode (Graph *g, void *data)
 	n->links = LIS_CriarLista(NULL);
 	n->data = data;
 	IrFinalLista( g->nodes );
-	if (!LIS_InserirElementoApos( g->nodes , n)){
+	if ( LIS_CondRetOK != LIS_InserirElementoApos( g->nodes , n) ){
 		free( n );
 		return graphMemoryError;
 	}
@@ -113,34 +114,43 @@ enum graphRet GraphDelNode (Graph *g)
 		return graphInvalidGraph;
 	if (!g->currentNode)
 		return graphInvalidCurrentNode
-	delNode(g->currentNode);
-	g->currentNode=0;
-	
+	delNode(g,g->currentNode);
+	g->currentNode = NULL;	
 }
-void delNode (void *n)
-{
-/* Essa funcao sera' passada na construcao da lista 'nodes'
- * pois a lista precisa apagar os nos.
- * Problema: Para apagar os nos e' necessario chamar (*g->delData).
- * Como trazer esse ponteiro ate aqui sem mudar a assinatura da
- * funcao?
- * 
+/* Houve um problema aqui: Como deletar Node?
+ *
+ * A lista 'nodes' poderia deletar 'node' com uma função que ela recebe por
+ * ponteiro, de assinatura void foo (void *). Mas pra deletar 'node' voce
+ * precisa deletar 'data', e pra isso você precisa do ponteiro de funcao
+ * que esta em cada instancia de graph. Como a funcao passada para a lista
+ * 'nodes' vai chamar essa funcao?
+ *
  * 1 - O modulo pode ter armazenado internamente todos os grafos
  * e ter uma funcao que procura um determinado no em todos os grafos
  * e o apaga. Essa funcao pode ser passada pra lista 'nodes'.
- * 
+ *
  * 2 - Cada no' pode possuir um ponteiro para o destrutor de data.
- * Assim, delNode pode executar:  (*n->DelData) (n->data);
+ * Assim, delNode pode executar:  (*n->delData) (n->data);
  *
  * 3 - Nao delegar a responsabilidade de apagar os nos a lista 'nodes':
  * Passar NULL na sua construcao e adicionar codigo para apagar 'data' em
- * todas as funcoes que podem apagar nos. 
- * 
+ * todas as funcoes que podem apagar nos.
+ *
  * Eu escolho o numero 3.
  */
 
-	LIS_DestruirLista (n->links);
-}
+// inline e' pra dar erro ao passar ponteiro da funcao
+inline void delNode (Graph *g,Node *n)
+{
+	LIS_DestruirLista( n->links );
+	( *g->delData )( n->data );		
+}	
+	
+
+
+
+
+
 enum graphRet GraphAddLink (Graph *g, Node *n)
 {
 }
