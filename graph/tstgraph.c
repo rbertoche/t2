c73c68d2ed4c7d58b8fc1965c568663dc542df8f
Raphael Bertoche <rbertoche@cpti.cetuc.puc-rio.br>
2010-10-09 03:30:28 -0300

    mais esboco para os testes, agora ja' uma ou outra funcao definida.

--- graph/tstgraph.c
+++ graph/tstgraph.c
@@ -10,7 +10,12 @@
 #include    "graph.h"
 
 static const int MAXGRAPHS = 10;
-pGraph vGraph[ MAXGRAPHS ];
+static const int MAXDATAS = 10;
+
+static pGraph vGraph[ MAXGRAPHS ];
+static char * vData[ MAXDATAS ];
+
+static int DataAlocCount = 0;
 
 static const char GRAPHNEW_CMD            [ ] = "=GraphNew"           ;
 static const char GRAPHDEL_CMD            [ ] = "=GraphDel"           ;
@@ -22,6 +27,57 @@ static const char GRAPHREMLINK_CMD        [ ] = "=GraphRemLink"       ;
 static const char GRAPHGETDATA_CMD        [ ] = "=GraphGetData"       ;
 static const char GRAPHGETSUCC_CMD        [ ] = "=GraphGetSuccessor"  ;
 
+static const char NEWDATA_CMD             [ ] = "=NewData"            ;
 
+void NewData ( char data )
+{
+	char *ret;
+	ret = (char*) malloc (sizeof(char));
+	DataAlocCount++;
+	*ret = data;
+	return red;
+}
+void DelData ( char * pdata )
+{
+	free (pdata);
+	DataAlocCount--;
+}
+
+TST_tpCondRet TST_EfetuarComando( char * CmdTeste )
+{
+	      if(! strcmp( CmdTeste == 		GRAPHNEW_CMD) ){
+
+      		argc = LER_LerParametros ( "i", &iGraph );
+		
+		if ( ! argc != 1 || argc < 0 || argc > MAXGRAPHS)
+			return TST_CondRetParm;
+		vGraph[iGraph] = GraphNew ( DelData );
+            	return TST_CompararPonteiroNulo( 1 , vGraph[iGraph],
+			"Erro: Ponteiro para novo grafo e' nulo." );
+
+	}else if(! strcmp( CmdTeste == 		GRAPHDEL_CMD) ){
+
+      		argc = LER_LerParametros ( "i", &iGraph );
+		
+		if ( ! argc != 1 || argc < 0 || argc > MAXGRAPHS)
+			return TST_CondRetParm;
+		GraphDel ( vGraph[iGraph] );
+		vGraph[iGraph] = NULL;
+
+            	return TST_CondRetOK;
+		
+	}else if(! strcmp( CmdTeste == 		GRAPHCCUR_CMD) ){
+
+		
+
+	}else if(! strcmp( CmdTeste == 		GRAPHNEWNODE_CMD) ){
+	}else if(! strcmp( CmdTeste == 		GRAPHDELNODE_CMD) ){
+	}else if(! strcmp( CmdTeste == 		GRAPHADDLINK_CMD) ){
+	}else if(! strcmp( CmdTeste == 		GRAPHREMLINK_CMD) ){
+	}else if(! strcmp( CmdTeste == 		GRAPHGETDATA_CMD) ){
+	}else if(! strcmp( CmdTeste == 		GRAPHGETSUCC_CMD) ){
+	}
+	
+}
 
 
