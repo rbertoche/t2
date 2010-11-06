f532cf98f71b70f8eb77a0ab8c53a70f8347568a
Raphael Bertoche <rbertoche@cpti.cetuc.puc-rio.br>
2010-11-06 04:03:16 -0200

    primeiro esboco do header do netmgr. Nem faz sentido tentar compilar ainda.

---
+++ Fontes/netmgr.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+*
+*	$MCD Modulo de definicao: Net Manager
+*
+*	Arquivo gerado:		netmgr.h
+*
+*	Autores: gac mpl rlb 
+*
+*	$ED Descricao do modulo
+*
+***************************************************************************/
+
+
+#ifndef GRAORE_
+
+enum interest {
+	WORK,
+	FRIENDS,
+	SEX,
+	NONE
+}
+
+/* Caso esse modulo va' gerar muitas strings, me parece fazer sentido que
+ele nao retorne condicoes de retorno em enum, mas em strings com mensagens de erro
+ou com NULL ou com a informacao pedida.
+
+Dessa forma concentramos codigo que manipula essas strings nesse modulo ao inves
+de empurrar pra outro.
+*/
+
+condRet NetNewUser (char *id);
+
+void NetDelMe();
+
+condRet NetEditMe();
+/* Contem codigo de input para editar o perfil*/
+
+condRet NetSearch (int isFriend, char *id, enum interest in, int minAge, int maxAge); 
+/* Pode cuspir varias linhas em stdout */
+/* posivelmente adicionar uma string com regexp sobre o nome mais tarde */
+
+condRet NetAddFriend (char *id);
+
+condRet NetUnfriend (char *id);
+
+condRet NetWrite (int destC, char * destV);
+/* Contem codigo de input para editar a mensagem*/
+
+condRet NetRead (char * sender);
+/* Pode cuspir varias linhas em stdout */
+
+condRet NetDelMsg (int msgNumber);
+
+
+
+#define GRAORE_
+
+
+
+
+
+
+
+
+
+#endif /* GRAORE_ */
+
