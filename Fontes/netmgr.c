5d8763b4d08e05aaf160c6c2dc2ef5ff4d111b06
Raphael Bertoche <rbertoche@cpti.cetuc.puc-rio.br>
2010-11-10 01:37:00 -0200

    Implementando NetAddFriend e NetUnfriend e passando a salvar ponteiro pro usuario corrente.

--- Fontes/netmgr.c
+++ Fontes/netmgr.c
@@ -4,16 +4,21 @@
 #include "graph.h"
 
 
-static const char NEWUSEROK[  ] = "Novo usuario criado.\n";
-static const char NEWUSERUSEDID[  ] = "ID escolhido ja' em uso.\n";
-static const char NETDELMEOK[  ] = "Usuario excluido com sucesso.\n";
+static const char NEWUSER_OK[  ] = "Novo usuario criado.\n";
+static const char NEWUSER_USEDID[  ] = "ID escolhido ja' em uso.\n";
+static const char NETDELME_OK[  ] = "Usuario excluido com sucesso.\n";
+static const char NETADDFRIEND_OK[  ] = "Amigo adicionado com sucesso.\n";
+static const char NETADDFRIEND_NOTFOUND[  ] = "Usuario nao encontrado.\n";
+static const char NETUNFRIEND_OK[  ] = "Amigo removido com sucesso.\n";
+static const char NETUNFRIEND_NOTFOUND[  ] = "Amigo nao encontrado.\n";
 
-int IsAuthenticated = 0;
+/* Atencao, cuidado ao deletar esse usr porque normalmente o grafo o deleta */
+Usr *usr = NULL;
 
 /* use essa funcao para acessar o ponteiro de grafo */
 int NetIsAuthenticated()
 {
-	return IsAuthenticated;
+	return usr?1:0;
 }
 
 pGraph getGraphInstance()
@@ -24,27 +29,46 @@ pGraph getGraphInstance()
 	return Net;
 }
 
-const char* NetNewUser (char *id)
+Usr *searchUsr(char *id)
 {
 	Usr* u;
 	GraphNodesStart(getGraphInstance());
 	u = GraphNodesGetNext (getGraphInstance());
 	while (u){
 		if(0 == strcmp(u->id,id))
-			return NEWUSERUSEDID;
+			return u;
 		u = GraphNodesGetNext (getGraphInstance());
 	}
-	u = UsrNew(id);
-	GraphNewNode( getGraphInstance(), u);
-	IsAutheticated = 1;
-	return NEWUSEROK;
+	return NULL;
+}
+
+Usr *searchLink(char *id)
+{
+	Usr *u;
+	GraphLinksStart(getGraphInstance());
+	u = GraphLinksGetNext (getGraphInstance());
+	while (u){
+		if(0 == strcmp(u->id,id))
+			return u;
+		u = GraphLinksGetNext (getGraphInstance());
+	}
+	return NULL;
+}
+
+const char* NetNewUser (char *id)
+{
+	if(searchUsr(id))
+		return NEWUSER_USEDID;
+	usr = UsrNew(id);
+	GraphNewNode( getGraphInstance(), usr);
+	return NEWUSER_OK;
 }
 
 const char* NetDelMe()
 {
 	GraphDelNode (getGraphInstance());
-	IsAutheticated = 0;
-	return NETDELMEOK;
+	usr = NULL;
+	return NETDELME_OK;
 }
 
 const char* NetEditMe()
@@ -54,12 +78,22 @@ const char* NetEditMe()
 
 const char* NetAddFriend (char *id)
 {
-	return NULL;
+	Usr *u;
+	u = searchUsr(id);
+	if (!u)
+		return NETADDFRIEND_NOTFOUND;
+	GraphAddLink(getGraphInstance(),u);
+	return NETADDFRIEND_OK;
 }
 
 const char* NetUnfriend (char *id)
 {
-	return NULL;
+	Usr *u;
+	u = searchLink(id);
+	if (!u)
+		return NETUNFRIEND_NOTFOUND;
+	GraphRemLink(getGraphInstance(),u);
+	return NETUNFRIEND_OK;
 }
 
 const char* NetWrite (int destC, char * destV)
