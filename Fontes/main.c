f8322c2cf2b41b760b255ad7106622185c47f891
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2010-11-15 20:56:45 -0200

    Main compila e roda alguns comandos.
    o comando login apenas loga o ultimo usuario criado.

--- Fontes/main.c
+++ Fontes/main.c
@@ -4,28 +4,32 @@
 #include "netmgr.h"
 #include "usr.h"
 
+int handle_commands (void);
 const char *cmd_exit (int argc, const char *argv[]);
-const char *cmd_login (int argc, const char *argv[]);
 
-int uhandle_commands (void);
 const char *ucmd_newUser (int argc, const char *argv[]);
 const char *invalid_command (int argc, const char *argv[]);
 const char *ucmd_error (int argc, const char *argv[]);
 const char *ucmd_help (int argc, const char *argv[]);
+const char *ucmd_create (int argc, const char *argv[]);
+const char *ucmd_login (int argc, const char *argv[]);
 
-int lhandle_commands (void);
 const char *lcmd_error (int argc, const char *argv[]);
 const char *lcmd_help (int argc, const char *argv[]);
+const char *lcmd_logoff (int argc, const char *argv[]);
+const char *lcmd_addfriend (int argc, const char *argv[]);
 
 struct cli_cmd_tuple unlogged_cmds[] = {
-	{ "login", &cmd_login },
+	{ "login", &ucmd_login },
 	{ "exit", &cmd_exit },
+	{ "create", &ucmd_create },
 	{ "help", &ucmd_help },
 	{ NULL, &ucmd_error },
 };
 
 struct cli_cmd_tuple logged_cmds[] = {
-	{ "logoff", NULL },
+	{ "logoff", &lcmd_logoff },
+	{ "addfriend", &lcmd_addfriend },
 	{ "exit", &cmd_exit },
 	{ NULL, &lcmd_error },
 };
@@ -33,8 +37,7 @@ struct cli_cmd_tuple logged_cmds[] = {
 char cmd_buff[1024];
 char ans_buff[1024];
 const char *ans_ptr = NULL;
-
-int (*handle_commands) (void) = &uhandle_commands;
+int logged = 0;
 
 int main (int argc, char *argv[])
 {
@@ -45,33 +48,38 @@ int main (int argc, char *argv[])
 	return 0;
 }
 
-const char *cmd_login (int argc, const char *argv[])
-{
-	/* TODO: checa se usuario existe, se sim, registra logged_cmds e
-	 * troca handle_commands para lhandle_commands.
-	 */
-	return NULL;
-}
-
 const char *cmd_exit (int argc, const char *argv[])
 {
 	exit (0);
 }
 
-/* ------------------------------------------------------------------------- */
-/* Unlogged Commands */
-/* ------------------------------------------------------------------------- */
-
-int uhandle_commands (void)
+int handle_commands (void)
 {
 	while (1){
-		printf ("please enter login: ");
+		if (logged){
+			/* TODO: WhoAmI aqui */
+			printf ("%s $ ", "<name>");
+		} else {
+			printf ("enter admin cmd or login: " );
+		}
 		if (fgets (cmd_buff, sizeof (cmd_buff), stdin) == NULL)
 			return 0;
 		ans_ptr = cli_call (cmd_buff);
 		if (ans_ptr != NULL)
 			printf ("%s", ans_ptr);
 	}
+}
+
+/* ------------------------------------------------------------------------- */
+/* Unlogged Commands */
+/* ------------------------------------------------------------------------- */
+
+const char *ucmd_create (int argc, const char *argv[])
+{
+	if (argc != 2){
+		return ucmd_help (0, NULL);
+	}
+	return NetNewUser (argv[1]);
 }
 
 const char *ucmd_error (int argc, const char *argv[])
@@ -83,28 +91,29 @@ const char *ucmd_error (int argc, const char *argv[])
 
 const char *ucmd_help (int argc, const char *argv[])
 {
-	printf ("lista de possiveis comandos:\n"
-			"+ login <user_name>\n"
-			"+ create_user <user_name>\n"
-			"+ del_user <user_name>\n");
+	printf ("lista de comandos:\n"
+			"\t+ login <user_name>\n"
+			"admin:\n"
+			"\t+ create <user_name>\n"
+			"\t+ delete <user_name>\n");
 	return NULL;
 }
+
+const char *ucmd_login (int argc, const char *argv[])
+{
+	if (NetIsAuthenticated()){
+		logged = 1;
+		cli_register_tuple (logged_cmds);
+		sprintf (ans_buff, "login as '%s' succeded\n", argv[1]);
+	} else {
+		sprintf (ans_buff, "login as '%s' failed\n", argv[1]);
+	}
+	return ans_buff;
+}
+
 /* ------------------------------------------------------------------------- */
 /* Logged Commands */
 /* ------------------------------------------------------------------------- */
-
-int lhandle_commands (void)
-{
-	while (1){
-		/* TODO: Imprime o nome do usuario com a funcao WhoAmI */
-		printf ("<user_name> $ ");
-		if (fgets (cmd_buff, sizeof (cmd_buff), stdin) == NULL)
-			return 0;
-		ans_ptr = cli_call (cmd_buff);
-		if (ans_ptr != NULL)
-			printf ("%s", ans_ptr);
-	}
-}
 
 const char *lcmd_error (int argc, const char *argv[])
 {
@@ -117,3 +126,21 @@ const char *lcmd_help (int argc, const char *argv[])
 	printf ("lista de possiveis comandos:\n");
 	return NULL;
 }
+
+const char *lcmd_logoff (int argc, const char *argv[])
+{
+	logged = 0;
+	cli_register_tuple (unlogged_cmds);
+	/* TODO: UsrWhoAmI() aqui */
+	sprintf (ans_buff, "bye <user_name>\n");
+	return ans_buff;
+}
+
+const char *lcmd_addfriend (int argc, const char *argv[])
+{
+	if (argc != 2){
+		sprintf (ans_buff, "syntax is %s user_id\n", argv[0]);
+	}
+	return NetAddFriend ((char*)argv[1]);
+}
+
