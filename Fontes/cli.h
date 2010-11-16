d575225806ea55b6ae99ee7c2e8bb4a5378a94ef
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2010-11-16 01:14:28 -0200

    Adicionados comentarios do "arcabouco".

--- Fontes/cli.h
+++ Fontes/cli.h
@@ -3,16 +3,16 @@
 
 /***************************************************************************
 *
-*	$MCD MÃ³dulo de definiÃ§Ã£o: Cli
+*	$MCD Módulo de definição: Cli
 *
 *	Arquivo gerado:		cli
 *
 *	Autores: rlbgacmp
 *
-*	$HA HistÃ³rico de evoluÃ§Ã£o:
+*	$HA Histórico de evolução:
 *		Vide repositorio Git
 *
-*	$ED DescriÃ§Ã£o do mÃ³dulo
+*	$ED Descrição do módulo
 *           Módulo de implementação da linha de comando responsável
 *           por todas alterações na rede de relacionamentos
 ***************************************************************************/
@@ -20,7 +20,7 @@ typedef const char * (*fncallback) (int argc, const char *argv[]);
 
 /***********************************************************************
 *	$TC Tipo de Dados: cli_cmd_tuple
-*	$ED DescriÃ§Ã£o do tipo
+*	$ED Descrição do tipo
 *		struct utilizada para criar um registro de comando
 ***********************************************************************/
 struct cli_cmd_tuple {
@@ -33,12 +33,12 @@ struct cli_cmd_tuple {
 *
 *	$EP Parametros
 *		$P s - ponteiro para string de comandos.
-*			
-*	$ED DescriÃ§Ã£o da funÃ§Ã£o
+*
+*	$ED Descrição da função
 *		        Le e interpreta uma linha de comando.
-*               Tokeniza a string e executa a funcao registrada 
-*               correspondente ao comando. Retorna -1 caso o comando nao 
-*               exista ou o retorno do comando caso contrario. 
+*               Tokeniza a string e executa a funcao registrada
+*               correspondente ao comando. Retorna -1 caso o comando nao
+*               exista ou o retorno do comando caso contrario.
 *
 *	$FV Valor retornado
 *		        -1 caso o comando nao exista ou o retorno do comando caso
@@ -54,10 +54,10 @@ const char *cli_call (char *s);
 *
 *	$EP Parametros
 *		$P cmds - ponteiro para estrutura de registro de comando.
-*			
-*	$ED DescriÃ§Ã£o da funÃ§Ã£o
-*               Registra um vetor de registros de comandos. Esse vetor 
-*               contem o mapeamento dos comandos para as funcoes que 
+*
+*	$ED Descricao da funcao
+*               Registra um vetor de registros de comandos. Esse vetor
+*               contem o mapeamento dos comandos para as funcoes que
 *               respondem por eles.
 
 *
