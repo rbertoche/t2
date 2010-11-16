2e32fa1278379d7624f9a42ae7059aae6239f3b5
Gabriel <gabriel_ac_1@hotmail.com>
2010-11-15 23:12:59 -0200

    Mudando comentários do cli.h para arcabouço style

--- Fontes/cli.h
+++ Fontes/cli.h
@@ -1,32 +1,67 @@
 #ifndef __CLI_H_
 #define __CLI_H_
 
-/* Tipo dos ponteiros das funcoes a serem registradas com comandos
- * Os argumentos na linha de comando lida sao tokenizados:
- * argc vale o numero de argumentos e argv um vetor contendo os
- * argumentos em strings */
+/***************************************************************************
+*
+*	$MCD Módulo de definição: Cli
+*
+*	Arquivo gerado:		cli
+*
+*	Autores: rlbgacmp
+*
+*	$HA Histórico de evolução:
+*		Vide repositorio Git
+*
+*	$ED Descrição do módulo
+*           M�dulo de implementa��o da linha de comando respons�vel
+*           por todas altera��es na rede de relacionamentos
+***************************************************************************/
 typedef const char * (*fncallback) (int argc, const char *argv[]);
 
-/* struct utilizada para criar um registro de comando
- * s deve conter a string do comando
- * cb a funcao a ser executada. */
+/***********************************************************************
+*	$TC Tipo de Dados: graphRet
+*	$ED Descrição do tipo
+*		struct utilizada para criar um registro de comando
+***********************************************************************/
 struct cli_cmd_tuple {
 	char *s;
 	fncallback cb;
 };
+/***********************************************************************
+*
+*	$FC Funcao: cli_call
+*
+*	$EP Parametros
+*		$P s - ponteiro para string de comandos.
+*			
+*	$ED Descrição da função
+*		        Le e interpreta uma linha de comando.
+*               Tokeniza a string e executa a funcao registrada 
+*               correspondente ao comando. Retorna -1 caso o comando nao 
+*               exista ou o retorno do comando caso contrario. 
+*
+*	$FV Valor retornado
+*		        -1 caso o comando nao exista ou o retorno do comando caso
+*               contr�rio.
+*
+***********************************************************************/
 
-/* Le e interpreta uma linha de comando.
- * Tokeniza a string e executa a funcao registrada correspondente ao comando.
- * Retorna -1 caso o comando nao exista ou o retorno do comando caso
- * contrario. */
 const char *cli_call (char *s);
 
-/* Registra um vetor de registros de comandos. Esse vetor contem o mapeamento
- * dos comandos para as funcoes que respondem por eles.
- *
- * Assertiva estrutural:
- * - Esse vetor deve ser terminado por { NULL, foo }.
- *   foo e' chamada quando nao existe mach de nenhum dos comandos acima. */
+ /***********************************************************************
+*
+*	$FC Funcao: cli_register_tuple
+*
+*	$EP Parametros
+*		$P cmds - ponteiro para estrutura de registro de comando.
+*			
+*	$ED Descrição da função
+*               Registra um vetor de registros de comandos. Esse vetor 
+*               contem o mapeamento dos comandos para as funcoes que 
+*               respondem por eles.
+
+*
+***********************************************************************/
 void cli_register_tuple (const struct cli_cmd_tuple *cmds);
 
 
