b53ad89ecbb79e07f47e8474adf9421fc46354c7
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2010-11-16 03:14:00 -0200

    UsrDelMsg -> UsrMsgDel em netmgr.c

--- Fontes/netmgr.c
+++ Fontes/netmgr.c
@@ -179,7 +179,7 @@ const char* NetRead (int msgNumber)
 
 const char* NetDelMsg (int msgNumber)
 {
-	if (!UsrDelMsg(usr, msgNumber))
+	if (!UsrMsgDel(usr, msgNumber))
 		return NETDELMSG_OK;
 	return NETDELMSG_NOTFOUND;
 }
