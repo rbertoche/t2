c77b73316cf1c128a2dfcb9d60e213a42513ade0
Raphael Bertoche <rbertoche@cpti.cetuc.puc-rio.br>
2010-12-13 16:22:30 -0300

    Mudando batch pra sempre rodar todos os testes, mesmo que algum falhe (cherry picked from commit 8bcb033163ad50af9909f628b8af522df6ee9815)

--- Batches/TestaTudo.bat
+++ Batches/TestaTudo.bat
@@ -5,24 +5,16 @@ cls
 if exist ..\scripts\estatisticas.estat  del ..\scripts\estatisticas.estat
 
 ..\produto\testelista        /s..\scripts\testelista             /l..\produto\testelista              /a..\scripts\estatisticas
-if errorlevel 4 goto sai
 
 ..\produto\testgraph        /s..\scripts\testgraph             /l..\produto\testgraph              /a..\scripts\estatisticas
-if errorlevel 4 goto sai
 
-..\produto\testgraph        /s..\scripts\testgraph             /l..\produto\testgraphdbg              /a..\scripts\estatisticas
-if errorlevel 4 goto sai
+..\produto\testgraphdbg        /s..\scripts\testgraph             /l..\produto\testgraphdbg              /a..\scripts\estatisticas
 
 ..\produto\testusr        /s..\scripts\testeusr             /l..\produto\testusr              /a..\scripts\estatisticas
-if errorlevel 4 goto sai
 
 ..\produto\testcli        /s..\scripts\testecli             /l..\produto\testcli              /a..\scripts\estatisticas
-if errorlevel 4 goto sai
 
 ..\produto\testnetmgr     /s..\scripts\testenetmgr             /l..\produto\testnetmgr              /a..\scripts\estatisticas
-if errorlevel 4 goto sai
-
-..\..\ferramnt\exbestat /e..\scripts\estatisticas
 
 :sai
 
